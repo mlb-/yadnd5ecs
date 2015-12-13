eb96e556143c498904f0a8925facd5a66bf8fe59
Matthew Batema <MBatema@trustwave.com>
2015-12-13 15:42:40 -0800

    Add name for player and character

--- src/cljs/yadnd5ecs/core.cljs
+++ src/cljs/yadnd5ecs/core.cljs
@@ -5,12 +5,29 @@
 
 (enable-console-print!)
 
-(def app-state (atom {:text "Hello Chestnut!"}))
+(def app-state (atom {:name {:player "Bob"
+                             :character "Bruenor"}}))
+
+(defcomponent name-view
+  [name owner]
+  (render
+   [_]
+   (let [{:keys [player
+                 character]} name]
+     (dom/span
+      (dom/h1 (str "Name: " player))
+      (dom/h1 (str "Character: " character))))))
 
 (defcomponent app-view
   [app owner]
-  (render [_]
-          (dom/h1 (:text app))))
+  (render
+   [_]
+   (dom/div
+    (dom/h1 (str "Hello " (-> app :name :character)
+                 (when-let [name (-> app :name :player)]
+                   (str " (" name ")"))
+                 "!"))
+    (om/build name-view (:name app)))))
 
 (defn main []
   (om/root
